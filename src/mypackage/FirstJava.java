bbcfa8676173fe03ea68bb084307d0367c19d3e5
Vaishnavi Patil <161804706+Vaishnavi0365@users.noreply.github.com>
2026-01-26 21:14:49 +0530

    Update FirstJava.java

--- src/mypackage/FirstJava.java
+++ src/mypackage/FirstJava.java
@@ -4,15 +4,14 @@ public class FirstJava {
 
 	public static void main(String[] args) {
 		//This is my first java program
+		
 		/*this
 		 is 
 		 a multiline 
 		 program
 		 */
-		System.out.println("Hello world");
-		System.out.print("Hi\n");
-		System.out.print("Hey");
-		
+		System.out.println("Hello world");		
 	}
 
 }
+
