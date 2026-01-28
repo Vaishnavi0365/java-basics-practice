1d856a9316a038bb780e2fdabdb2e08b9fb77674
Vaishnavi Patil <161804706+Vaishnavi0365@users.noreply.github.com>
2026-01-29 01:48:41 +0530

    Add SubStringMethod email parser program

---
+++ src/mypackage/SubStringMethod.java
@@ -0,0 +1,33 @@
+package mypackage;
+
+import java.util.Scanner;
+
+public class SubStringMethod {
+	
+	public static void main (String[] args) {
+		
+		Scanner scanner = new Scanner (System.in);
+		
+		String email;
+		String username;
+		String domain;
+		
+		System.out.print("Enter your Email: ");
+		email = scanner.nextLine();
+		
+		if(email.contains("@")) {
+			username = email.substring(0 , email.indexOf("@"));
+			domain = email.substring(email.indexOf("@")+1);
+			
+			System.out.println("Your email: " + email);
+			System.out.println("Username: "+ username);
+			System.out.println("Domain: " + domain);
+			
+		} else {
+			
+			System.out.println("Email must contain @");
+		}
+		
+	}
+
+}
